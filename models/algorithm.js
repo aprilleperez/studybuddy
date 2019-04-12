cfc87f01d2822425c9397c3a547074859ff33f38
CGibeau <claire.gibeau@outlook.com>
2019-04-11 21:52:58 -0700

    added algorithm file

---
+++ models/algorithm.js
@@ -0,0 +1,9 @@
+//this is where the logic for finding study buddy matches will live
+
+//there will be an export function to return matches by user ID. 
+
+
+module.exports = function (userID) {
+//working on algorithm-return will change
+    return "userMatches IDs";
+}
