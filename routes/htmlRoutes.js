0e9c7797f1c3b7309e61e982005238753c5293f6
aprilleperez <aprillesakura@gmail.com>
2019-04-14 19:32:58 -0700

    able to test hbs with dummy routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,30 @@
 var db = require("../models");
 
+// var fakeArray = [{id:1, text:'someText'}, {id:1, text:'Some other burger text'}];
+
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    // db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+        authenticated: false // FOR TESTING 
       });
-    });
+    // });
+  });
+
+  app.get("/user/matches", function(req, res) {
+      res.render("matchpage", {
+        authenticated: true
+      });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
-        example: dbExample
+        example: 1 //fakeArray //dbExample
       });
-    });
+    // });
   });
 
   // Render 404 page for any unmatched routes
