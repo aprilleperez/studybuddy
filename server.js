fc018d5ee98e67323a89f1b167c77e222f7c3e66
CGibeau <claire.gibeau@outlook.com>
2019-04-15 17:40:45 -0700

    on local server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var session = require("express-session");
 
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
